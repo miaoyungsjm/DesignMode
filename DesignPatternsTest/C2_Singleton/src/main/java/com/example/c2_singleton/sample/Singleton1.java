3c6d2964c8b26b73101c1f2561965e5c54626b34
guozhu.gan <guozhu.gan@gotechcn.cn>
2020-07-21 10:23:51 +0800

    C2_Singleton - add explanation

--- DesignPatternsTest/C2_Singleton/src/main/java/com/example/c2_singleton/sample/Singleton1.java
+++ DesignPatternsTest/C2_Singleton/src/main/java/com/example/c2_singleton/sample/Singleton1.java
@@ -7,8 +7,9 @@ import java.io.Serializable;
  * @author ggz
  * @date 2020/7/21
  * <p>
- * 饿汉模式
- * 外加反序列化
+ * 饿汉模式 - 外加反序列化
+ * 基于 classloader 机制避免了多线程的同步问题
+ * 类加载时就初始化，浪费内存
  */
 public class Singleton1 implements Serializable {
 
